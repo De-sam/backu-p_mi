4e16e483fea35401245766f6ab572233999f38d1
De-sam <samsoncoded@gmail.com>
2024-01-23 05:18:57 +0100

    my set up back up

---
+++ clone_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install build-essential
+sudo apt install python3-pip
+sudo apt install valgrind
+sudo apt install mysql-server
+cd ..
+git clone https://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@github.com/De-sam/alx-system_engineering-devops.git
+git clone https://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@github.com/De-sam/alx-higher_level_programming.git
+git clone https://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@github.com/De-sam/alx-low_level_programming.git
+git clone https://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@github.com/De-sam/AirBnB_clone.git
