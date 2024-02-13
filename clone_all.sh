3506c631bf616f1d9996a3c5b0062553ca698166
De-sam <samsoncoded@gmail.com>
2024-02-13 11:21:37 +0100

    backup updated

--- clone_all.sh
+++ clone_all.sh
@@ -5,8 +5,23 @@ sudo apt install build-essential
 sudo apt install python3-pip
 sudo apt install valgrind
 sudo apt install mysql-server
-cd ..
+cd
+mkdir ALX
+cd ALX
 git clone https://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@github.com/De-sam/alx-system_engineering-devops.git
 git clone https://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@github.com/De-sam/alx-higher_level_programming.git
 git clone https://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@github.com/De-sam/alx-low_level_programming.git
 git clone https://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@github.com/De-sam/AirBnB_clone.git
+mkdir CLONE
+cd CLONE
+mkdir emma
+cd emma
+git clone https://github.com/Adedoyin-Emmanuel/alx-higher_level_programming.git
+git clone https://github.com/Adedoyin-Emmanuel/alx-low_level_programming.git
+git clone https://github.com/Adedoyin-Emmanuel/alx-system_engineering-devops.git
+cd ..
+mkdir copy2
+cd copy2
+git clone https://github.com/Abstaina44/alx-low_level_programming.git
+git clone https://github.com/Abstaina44/alx-higher_level_programming.git
+git clone https://github.com/Abstaina44/alx-system_engineering-devops.git
